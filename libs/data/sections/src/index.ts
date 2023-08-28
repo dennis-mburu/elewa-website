ba347a59752074f20a9d5e0f5b617915750d1e08
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-28 22:50:01 +0300

    chore: add mock highlighted news data on libs/data/sections folder

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/highlighted-project.data';
+export * from './lib/mock-highlighted-news.data';
+
---
+++ libs/data/sections/src/lib/mock-highlighted-news.data.ts
@@ -0,0 +1,18 @@
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
+export const __highlightedNews: NewsItem[] = [
+  {
+    title: ' Conversational learning ipsum dolar',
+    description: 'Eos qui ratione voluptatem sequi nesciunt.',
+    buttonText: 'Read more',
+  },
+  {
+    title: ' Conversational learning ipsum dolar',
+    description: 'Eos qui ratione voluptatem sequi nesciunt.',
+    buttonText: 'Read more',
+  },
+  {
+    title: ' Conversational learning ipsum dolar',
+    description: 'Eos qui ratione voluptatem sequi nesciunt.',
+    buttonText: 'Read more',
+  },
+];
