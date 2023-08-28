0a3593035c1c2131d429bb2265d857cb7698673d
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-28 22:58:23 +0300

    :sparkles: import card module on home page and loop through mock news data to render news section on home page

--- libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
+++ libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
+import { __highlightedNews } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-news-section',
   templateUrl: './news-section.component.html',
   styleUrls: ['./news-section.component.scss'],
 })
-export class NewsSectionComponent {}
+export class NewsSectionComponent implements OnInit {
+  @Input() newsData!: NewsItem[];
+
+  ngOnInit(): void {
+    this.newsData = __highlightedNews;
+  }
+}
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './main/home/home-page.component';
+import { CardsModule } from '@elewa-website/elements/cards';
 
-import { HomeRoutingModule } from './home.routing';
+import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
 
+import { HomeRoutingModule } from './home.routing';
+
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, CardsModule],
   declarations: [HomePageComponent, NewsSectionComponent],
   exports: [HomePageComponent],
 })
